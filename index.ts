c9db91ca6b0ce482f735cd6e8dd151ee3ecc3202
junaidkhen <160384541+junaidkhen@users.noreply.github.com>
2024-03-23 12:32:43 +0500

    created number guessing game

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomeNumber = Math.floor(Math.random() * 6 + 1);
+
+const answer = await inquirer.prompt([
+  {
+    name: "userGuessNumber",
+    type: "number",
+    message: "Please guess a number from 1 to 6: ",
+  },
+]);
+
+if (answer.userGuessNumber === randomeNumber) {
+  console.log("Congratz! you guess correct number.");
+} else {
+  console.log("You guess wrong number.");
+}
+
